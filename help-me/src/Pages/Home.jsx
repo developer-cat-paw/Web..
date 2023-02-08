b7ab0aa555f446b693df7de31c9f6f34ada5559a
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2023-02-08 13:58:15 +0900

    last

--- help-me/src/Pages/Home.jsx
+++ help-me/src/Pages/Home.jsx
@@ -21,47 +21,39 @@ const Home = () => {
                     <div className='name'>이경민</div>
                     <div className='tag-and'>And</div>
                     <div className='text'>
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
-                        개발하기싫어 개발하기싫어 개발하기싫어 개발하기싫어
+                        개발하기싫어
                     </div>
                 </div>
                 <div className='texts2'>
                     <img className='profile' src={profile2}></img>
                     <div className='name'>김명준</div>
                     <div className='tag-web'>Web</div>
-                    <div className='text'>안녕안녕 ~~</div>
+                    <div className='text'>같이 Vue.js공부 해보실분?? 제가 알려드릴수 있어요 !! 편하게 연락주세요</div>
                 </div>
                 <div className='texts3'>
                     <img className='profile' src={profile2}></img>
                     <div className='name'>장범준</div>
                     <div className='tag-sever'>Web</div>
-                    <div className='text'>안녕 나는 대소고 백앤드 1짱 나한테 파이버베이스 배울 사람 일로와 ~~</div>
+                    <div className='text'>같이 firebase 공부 해보실분 있나욧??? firebase공부하면 풀스택 쌉가능임 ㅋㅋ!!</div>
                 </div>
                 <div className='texts4'>
                     <img className='profile' src={profile2}></img>
                     <div className='name'>황주완</div>
                     <div className='tag-ai'>Ai</div>
-                    <div className='text'>안녕 나는 남자가 좋아 나랑 공부할사람 ~?</div>
+                    <div className='text'>같이 ai챗봇 공부해보실분 있나요?? ai에 대해서궁금하신분들 연락 주세요!!</div>
                 </div>
                 <div className='texts5'>
                     <img className='profile' src={profile2}></img>
                     <div className='name'>최시훈</div>
                     <div className='tag-ios'>Ios</div>
-                    <div className='text'>
-                        
-                        
+                    <div className='text'>애플 좋아 애플짱
                     </div>
                 </div>
                 <div className='texts6'>
-                <img className='profile' src={profile2}></img>
+                    <img className='profile' src={profile2}></img>
                     <div className='name'>모성현</div>
                     <div className='tag-deg'>Degsign</div>
-                    <div className='text'>나는 트렌스젠더가 좋아 나랑 섹스 할사람?????
-                        DM !!!!!!!! 빨리 !! 나급해 !!!!!!
+                    <div className='text'>좋은 디자인 초안 있습니다 같이 프로젝트 하실분 연락 ㄱㄱ
                     </div>
                 </div>
             </div>
